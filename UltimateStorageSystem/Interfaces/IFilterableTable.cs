7a16f39f882d02ccf5d4079e3335e6a7fd4436fe
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-09-12 12:31:22 -0700

    add: missing interface classes

---
+++ UltimateStorageSystem/Interfaces/IFilterableTable.cs
@@ -0,0 +1,7 @@
+﻿namespace UltimateStorageSystem.Interfaces;
+public interface IFilterableTable
+{
+    void FilterItems(string searchText);
+    void SortItemsBy(string sortBy, bool ascending);
+    int  ScrollIndex { get; set; }
+}
---
+++ UltimateStorageSystem/Interfaces/IScrollableTable.cs
@@ -0,0 +1,7 @@
+﻿namespace UltimateStorageSystem.Interfaces;
+public interface IScrollableTable
+{
+    int ScrollIndex { get; set; }
+    int GetItemEntriesCount();
+    int GetVisibleRows();
+}
